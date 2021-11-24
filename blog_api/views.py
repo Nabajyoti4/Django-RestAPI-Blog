ca85fcaf1fc002f9ead880300ef1e60a323f4283
Nabajyoti4 <sirnaba@gmail.com>
2021-11-24 09:15:16 +0530

    djnago blog complete with trst cases

---
+++ blog_api/views.py
@@ -0,0 +1,12 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from blog.models import Post
+from .serializers import PostSerializer
+
+class PostList(ListCreateAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
